d14bc36e4369ef9c6afe35099ea2224113c9fedd
Ludvig Grenmo <ludvig.grenmo@adlibris.com>
2023-08-26 19:47:06 +0200

    first stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,263 @@
-import Image from 'next/image'
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+	Card,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
+import { Separator } from '@/components/ui/separator';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+
+import { useForm } from 'react-hook-form';
+
+import * as z from 'zod';
+
+const formSchema = z.object({
+	time: z.string(),
+	distance: z.string(),
+	pace: z.string(),
+	unit: z.string(),
+	minUnit: z.string(),
+});
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			unit: 'km',
+			minUnit: 'min/km',
+			time: '',
+			pace: '',
+			distance: '',
+		},
+	});
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const [error, setError] = useState('');
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		const { time, distance, pace, unit, minUnit } = values;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		const distanceMeasure = unit === 'km' ? 1 : 1.60934;
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+		if (time && distance && pace) {
+			setError('The values does not add upp. Try leaving one field empty');
+			return;
+		}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+		if (time && distance) {
+			const finalPace = calculatePace(time, Number(distance) * distanceMeasure);
+			form.setValue('pace', finalPace);
+			setError('');
+		}
+
+		if (time && pace) {
+			const finalDistance = calculateDistance(time, pace);
+			form.setValue('distance', finalDistance);
+			setError('');
+		}
+
+		if (pace && distance) {
+			const finalTime = convertPaceAndDistanceToTime(
+				pace,
+				Number(distance) * distanceMeasure
+			);
+			if (!finalTime) return null;
+			setError('');
+
+			form.setValue('time', finalTime);
+		}
+	}
+
+	return (
+		<main className="bg-hero-pattern min-h-screen flex gap-4 justify-center flex-col items-center">
+			<Form {...form}>
+				<div className="bg-black bg-opacity-75  p-10 rounded-md">
+					<div>
+						<CardHeader>
+							<CardTitle className="text-4xl">Cardio calculator</CardTitle>
+							<CardDescription>
+								Fill in 2 fields to generate your data
+							</CardDescription>
+						</CardHeader>
+
+						<Separator />
+					</div>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="flex flex-col gap-4 mt-4"
+					>
+						<FormField
+							control={form.control}
+							name="time"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Time</FormLabel>
+									<FormControl>
+										<Input placeholder="00:00:00" {...field} />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<div className="flex w-full gap-2 items-end justify-between">
+							<FormField
+								control={form.control}
+								name="distance"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Distance</FormLabel>
+										<FormControl>
+											<Input placeholder="0" {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="unit"
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Select
+												onValueChange={field.onChange}
+												defaultValue={field.value}
+											>
+												<SelectTrigger>
+													<SelectValue {...field} placeholder="Unit" />
+												</SelectTrigger>
+												<SelectContent>
+													<SelectItem value={'km'}>km</SelectItem>
+													<SelectItem value={'mi'}>mi</SelectItem>
+												</SelectContent>
+											</Select>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</div>
+						<div className="flex w-full gap-2 items-end justify-between">
+							<FormField
+								control={form.control}
+								name="pace"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Pace</FormLabel>
+										<FormControl>
+											<Input placeholder="00:00" {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="minUnit"
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Select
+												onValueChange={field.onChange}
+												defaultValue={field.value}
+											>
+												<SelectTrigger>
+													<SelectValue placeholder="Min/?" />
+												</SelectTrigger>
+												<SelectContent>
+													<SelectItem value={'min/km'}>min/km</SelectItem>
+													<SelectItem value={'min/mi'}>min/mi</SelectItem>
+												</SelectContent>
+											</Select>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</div>
+
+						<div className="flex gap-2">
+							<Button className="w-full" variant="secondary" type="submit">
+								Submit
+							</Button>
+							<Button
+								type="button"
+								onClick={() => {
+									form.reset();
+									setError('');
+								}}
+							>
+								Clear
+							</Button>
+						</div>
+					</form>
+					<CardFooter className="flex justify-between"></CardFooter>
+				</div>
+			</Form>
+			{error && <div className="text-gray-500">{error}</div>}
+		</main>
+	);
+
+	function convertPaceAndDistanceToTime(
+		pace: string,
+		distance: number
+	): string {
+		const [paceMinutes, paceSeconds] = pace.split(':').map(Number);
+
+		const paceInSeconds = paceMinutes * 60 + paceSeconds;
+		const totalTimeInSeconds = paceInSeconds * distance;
+
+		const hours = Math.floor(totalTimeInSeconds / 3600);
+		const minutes = Math.floor((totalTimeInSeconds % 3600) / 60);
+		const seconds = totalTimeInSeconds % 60;
+
+		const formattedTime = `${String(hours).padStart(2, '0')}:${String(
+			minutes
+		).padStart(2, '0')}:${String(seconds.toFixed(0)).padStart(2, '0')}`;
+
+		return formattedTime;
+	}
+
+	function calculateDistance(time: string, pace: string): string {
+		const [timeHours, timeMinutes, timeSeconds] = time.split(':').map(Number);
+		const [paceMinutes, paceSeconds] = pace.split(':').map(Number);
+
+		const totalTimeInSeconds =
+			timeHours * 3600 + timeMinutes * 60 + timeSeconds;
+		const paceInSeconds = paceMinutes * 60 + paceSeconds;
+
+		const distanceInKm = totalTimeInSeconds / paceInSeconds;
+		const formattedDistance = distanceInKm.toFixed(2);
+		return parseFloat(formattedDistance).toString();
+	}
+
+	function calculatePace(time: string, distance: number): string {
+		const [timeHours, timeMinutes, timeSeconds] = time.split(':').map(Number);
+
+		const totalTimeInSeconds =
+			timeHours * 3600 + timeMinutes * 60 + timeSeconds;
+		const paceInSeconds = totalTimeInSeconds / distance;
+
+		const paceMinutes = Math.floor(paceInSeconds / 60);
+		const paceSeconds = Math.round(paceInSeconds % 60);
+
+		const formattedPace = `${String(paceMinutes).padStart(2, '0')}:${String(
+			paceSeconds
+		).padStart(2, '0')}`;
+
+		return formattedPace;
+	}
 }
